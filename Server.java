ed72f4515af3f750b6e8999ee0cf0429305ac1ad
Satabdhi Reddy Nalamalpu <48411814+satabdhi@users.noreply.github.com>
2019-03-10 21:58:34 -0400

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,163 @@
+package com;
+
+import java.io.*;
+import java.net.*;
+import java.net.Socket;
+import java.util.*;
+;                   
+
+
+// Each Client Connection will be managed in a dedicated Thread
+public class JavaHTTPServer implements Runnable{
+
+    static final File WEB_ROOT = new File(".");
+
+ 
+
+    // verbose mode
+    static final boolean verbose = true;
+
+    // Client Connection via Socket Class
+    private Socket connect;
+
+    public JavaHTTPServer(Socket c) {
+        connect = c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter port number:");
+        Scanner s= new Scanner(System.in);
+          int PORT = Integer.parseInt(s.nextLine()) ;
+        try {
+            ServerSocket serverConnect = new ServerSocket(PORT);
+            System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+
+            // we listen until user halts server execution
+            while (true) {
+                JavaHTTPServer myServer = new JavaHTTPServer(serverConnect.accept());
+
+                if (verbose) {
+                    System.out.println("Connection opened. (" + new Date() + ")");
+                }
+
+                // create dedicated thread to manage the client connection
+                Thread thread = new Thread(myServer);
+                thread.start();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Server Connection error : " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        // we manage our particular client connection
+        BufferedReader in = null; PrintWriter out = null; BufferedOutputStream dataOut = null;
+        String fileRequested = null;
+
+        try {
+            // we read characters from the client via input stream on the socket
+            in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+            // we get character output stream to client (for headers)
+            out = new PrintWriter(connect.getOutputStream());
+            // get binary output stream to client (for requested data)
+            dataOut = new BufferedOutputStream(connect.getOutputStream());
+
+            // get first line of the request from the client
+            String input = in.readLine();
+            System.out.println(input);
+            // we parse the request with a string tokenizer
+            StringTokenizer parse = new StringTokenizer(input);
+            String method = parse.nextToken().toUpperCase(); // we get the HTTP method of the client
+            // we get file requested
+            fileRequested = parse.nextToken().toLowerCase();
+
+            // we support only GET and HEAD methods, we check
+            if (!method.equals("GET")  &&  !method.equals("HEAD")) {
+                if (verbose) {
+                    System.out.println("Error 501 Not Implemented : " + method + " method.");
+                }
+
+
+            } else {
+                // GET or PUT method
+
+
+                File file = new File(WEB_ROOT, fileRequested);
+                int fileLength = (int) file.length();
+                String content = getContentType(fileRequested);
+
+                if (method.equals("GET")) { // GET method so we return content
+                    byte[] fileData = readFileData(file, fileLength);
+
+                    // send HTTP Headers
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Server: Java HTTP Server ");
+                    out.println("Date: " + new Date());
+                    out.println("Content-type: " + content);
+                    out.println("Content-length: " + fileLength);
+                    out.println(); // blank line between headers and content, very important !
+                    out.flush(); // flush character output stream buffer
+
+                    dataOut.write(fileData, 0, fileLength);
+                    dataOut.flush();
+                }
+
+                if (true) {
+                    System.out.println("File " + fileRequested + " of type " + content + " returned");
+                }
+
+            }
+
+        } catch (FileNotFoundException fnfe) {
+            fileNotFound();
+
+
+        } catch (IOException ioe) {
+            System.err.println("Server error : " + ioe);
+        } finally {
+            try {
+                in.close();
+                out.close();
+                dataOut.close();
+                connect.close(); // we close socket connection
+            } catch (Exception e) {
+                System.err.println("Error closing stream : " + e.getMessage());
+            }
+
+                System.out.println("Connection closed.\n");
+
+        }
+
+
+    }
+
+    private byte[] readFileData(File file, int fileLength) throws IOException {
+        FileInputStream fileIn = null;
+        byte[] fileData = new byte[fileLength];
+
+        try {
+            fileIn = new FileInputStream(file);
+            fileIn.read(fileData);
+        } finally {
+            if (fileIn != null)
+                fileIn.close();
+        }
+
+        return fileData;
+    }
+
+    // return supported MIME Types
+    private String getContentType(String fileRequested) {
+        if (fileRequested.endsWith(".htm")  ||  fileRequested.endsWith(".html"))
+            return "text/html";
+        else
+            return "text/plain";
+    }
+
+    private void fileNotFound() {
+        System.out.println(" Error 404: NOT FOUND");
+    }
+
+}
